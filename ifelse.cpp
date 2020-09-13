ae4e36b3a8fbc7386a706643c0cbfb1320b61e75
vishwam-R <71185982+vishwam-R@users.noreply.github.com>
2020-09-13 11:08:02 +0530

    Added ifelse program

---
+++ ifelse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter Your Age:" << endl;
+    cin>>age;
+
+        if(age>18)
+            {
+                cout<<"Your Are Adult"<<endl;
+            }
+        else
+            {
+                cout<<"Your Are Not Adult"<<endl;
+            }
+    return 0;
+}
